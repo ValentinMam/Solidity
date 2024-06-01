880ffd05d79494246606687b955428feb588c5e7
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-06-01 17:27:19 +0200

    P2-C7

--- Partie-2/zombiefeeding.sol
+++ Partie-2/zombiefeeding.sol
@@ -12,4 +12,19 @@ import "./zombiefactory.sol";
 // Mettons cette logique dans sa propre classe qui hérite de toutes les méthodes de ZombieFactory.
 // 1.	Créez un contrat nommé ZombieFeeding en dessous de ZombieFactory. Ce contrat devra hériter de notre contrat ZombieFactory.
 
-contract ZombieFeeding is ZombieFactory {}
+contract ZombieFeeding is ZombieFactory {
+    // Il est temps de donner à nos zombies la capacité de se nourrir et de se multiplier !
+    // Quand un zombie se nourri d'une autre forme de vie, son ADN se combine avec l'autre forme de vie pour créer un nouveau zombie.
+    // 1.	Créez une fonction appelée feedAndMultiply qui aura deux paramètres : _zombieId (un uint) et _targetDna (aussi un uint).
+    // Cette fonction devra être public.
+    // 2.	Nous ne voulons pas que d'autres personnes puissent nourrir notre zombie !
+    // Nous allons vérifier que nous sommes bien le propriétaire de ce zombie.
+    // Ajoutez une déclaration require pour vérifier que msg.sender est égal au propriétaire du zombie (de la même manière que dans la fonction createRandomZombie).
+    // 3.	Nous allons avoir besoin de l'ADN de ce zombie.
+    // La prochaine chose que notre fonction devra faire c'est de déclarer un Zombie local nommé myZombie (qui sera un pointeur storage).
+    // Définissez cette variable égale à l'index _zombieId de notre tableau zombies.
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+    }
+}
