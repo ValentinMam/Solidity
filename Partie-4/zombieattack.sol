88a4c8e06751eaf5bbec64b3f83f28f72f1a398d
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-06-03 17:21:36 +0200

    P4-C3

---
+++ Partie-4/zombieattack.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.4;
+
+import "./zombiehelper.sol";
+
+// 1.	En haut du fichier, indiquez que nous utilisons la version ^0.4.19 de Solidity.
+// 2.	Importez avec import depuis zombiehelper.sol.
+// 3.	Déclarez un nouveau contract appelé ZombieBattle qui hérite de ZombieHelper. Laissez le corps du contrat vide pour l'instant.
+
+contract ZombieBattle is ZombieHelper {}
