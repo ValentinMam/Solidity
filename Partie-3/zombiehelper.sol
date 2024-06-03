ad3f1aca5adf65408679598a5b8b52481d100d78
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-06-03 12:07:43 +0200

    P3-C9

--- Partie-3/zombiehelper.sol
+++ Partie-3/zombiehelper.sol
@@ -12,4 +12,29 @@ contract ZombieHelper is ZombieFeeding {
         require(zombies[_zombieId].level >= _level);
         _;
     }
+    // 1.	Créez une fonction appelée changeName qui aura 2 paramètres : _zombieId (un uint), et _newName (un string), et la rendre external.
+    // Elle devra avoir le modificateur aboveLevel, et lui passer 2 pour l'argument _level. (N'oubliez pas de lui passer aussi le _zombieId).
+    // 2.	Dans cette fonction, premièrement nous allons vérifier que msg.sender est égal à zombieToOwner[_zombieId].
+    // Utilisez une déclaration require.
+    // 3.	Ensuite la fonction devra définir zombies[_zombieId].name égal à _newName.
+
+    function changeName(
+        uint _zombieId,
+        string _newName
+    ) external aboveLevel(2, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+    // 4.	Créez une autre fonction changeDna en dessous de changeName.
+    // Sa définition et son contenu seront presque identiques à changeName, excepté pour le second argument
+    // qui sera _newDna (un uint), et qu'elle devra passer 20 pour l'argument _level à aboveLevel.
+    // Et bien sur, elle devra définir le dna zombie égal à _newDna au lieu de définir le nom du zombie.
+
+    function changeDna(
+        uint _zombieId,
+        uint _newDna
+    ) external aboveLevel(20, _zombieId) {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
 }
