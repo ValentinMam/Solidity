151047ed443664e4b4a6b54e463e699b1bfb5c10
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-06-03 17:03:53 +0200

    P4-C1

--- Partie-4/zombiehelper.sol
+++ Partie-4/zombiehelper.sol
@@ -4,15 +4,27 @@ pragma solidity ^0.8.4;
 
 import "./zombiefeeding.sol";
 
+// Nous allons créer une fonction payable pour notre jeu de zombie.
+// Disons que notre jeu permet aux utilisateurs de faire passer un niveau à leurs zombies en payant des ETH.
+// Les ETH seront stockés dans le contrat, que vous possédez -
+// c'est un exemple simple de comment faire de l'argent avec vos jeux !
+
 contract ZombieHelper is ZombieFeeding {
-    // 1. Définissez levelUpFee ici
+    //   1.	Définissez un uint appelé levelUpFee égal à 0.001 ether.
+    uint levelUpFee = 0.001 ether;
 
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
     }
 
-    // 2. Ajoutez la fonction levelUp ici
+    // 2.	Créez une fonction appelée levelUp. Elle aura un paramètre, _zombieId, un uint. Elle devra être external et payable.
+    // 3.	La fonction devra d'abord utiliser un require pour vérifier que msg.value soit égal à levelUpFee.
+    // 4.	Elle devra ensuite incrémenter le level du zombie : zombies[_zombieId].level++.
+    function levelUp(uint _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
+    }
 
     function changeName(
         uint _zombieId,
