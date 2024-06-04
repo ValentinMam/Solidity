af872974a8ee4d62c167b94630deae9899f5c970
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-06-04 11:07:02 +0200

    P4-C9

--- Partie-4/zombiefactory.sol
+++ Partie-4/zombiefactory.sol
@@ -16,6 +16,11 @@ contract ZombieFactory is Ownable {
         uint dna;
         uint32 level;
         uint32 readyTime;
+        // 1.	Modifiez la structureZombie en lui ajoutant 2 propriétés :
+        //      a. winCount, un uint16
+        //      b. lossCount, aussi un uint16
+        uint16 winCount;
+        uint16 lossCount;
     }
 
     Zombie[] public zombies;
@@ -23,9 +28,13 @@ contract ZombieFactory is Ownable {
     mapping(uint => address) public zombieToOwner;
     mapping(address => uint) ownerZombieCount;
 
+    // 2.	Maintenant que nous avons de nouvelles propriétés pour notre structure Zombie,
+    // nous devons changer la définition de notre fonction _createZombie().
+    // Changez la définition de création de zombie pour que chaque nouveau zombie créé est 0 victoire et 0 défaite.
+
     function _createZombie(string _name, uint _dna) internal {
         uint id = zombies.push(
-            Zombie(_name, _dna, 1, uint32(now + cooldownTime))
+            Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0)
         ) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
