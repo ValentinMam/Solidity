0d818fd64bb3e7cd423e818b93261dea9762d1ae
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-06-04 14:36:47 +0200

    P5-C3

--- Partie-5/zombieownership.sol
+++ Partie-5/zombieownership.sol
@@ -17,4 +17,20 @@ pragma solidity ^0.8.4;
 import "./zombieattack.sol";
 import "./erc721.sol";
 
-contract ZombieOwnership is ZombieBattle, ERC721 {}
+contract ZombieOwnership is ZombieBattle, ERC721 {
+    function balanceOf(address _owner) public view returns (uint256 _balance) {
+        // 1.	Implémentez balanceOf pour renvoyer le nombre de zombies qu'un _owner a.
+        return ownerZombieCount[_owner];
+    }
+
+    function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+        //    2.	Implémentez ownerOf pour renvoyer l'adresse du propriétaire du zombie avec l'ID _tokenId.
+        return zombieToOwner[_tokenId];
+    }
+
+    function transfer(address _to, uint256 _tokenId) public {}
+
+    function approve(address _to, uint256 _tokenId) public {}
+
+    function takeOwnership(uint256 _tokenId) public {}
+}
